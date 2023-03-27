53eb69baab6509d376d4af6700488d37f446843a
Alla <allafaa@yandex.ru>
2023-03-27 22:36:13 +0300

    homework1

---
+++ main.py
@@ -0,0 +1,27 @@
+"""print ('enter first number: ')
+a = int(input ())
+b = int (input ('enter second number: '))
+print (a, '+', b, '=', a + b)
+
+
+c=5.89
+print (c)
+n=int(c)
+print (n)
+
+
+a = 5.989564
+b = 6.645674
+print (round(a*b, 3))"""
+
+"""a = 'qwerty'
+for i in a:
+    print (i)"""
+       
+
+line = ""
+for i in range(5):
+    line = ""
+    for j in range (5):
+        line += "*"
+    print (line)
